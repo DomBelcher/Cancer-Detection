27249bf8ea3bd71e84d468df6e8ffa70b79c5999
Dom Belcher <dominic.belcher@gmail.com>
2019-05-07 23:13:55 +0100

    updated neural network training to use new loader

--- pytorch/nn.py
+++ pytorch/nn.py
@@ -9,6 +9,7 @@ import torchvision.transforms as transforms
 
 from models.resnet_model_3a import TestModel
 from data_loader import loader
+from data_loader_2 import load_datasets
 import transforms as tfs
 from helpers import deactivate_layer
 
@@ -31,13 +32,15 @@ trans = transforms.Compose([
 batch_size = 128
 validation_split = .2
 shuffle_dataset = True
-p = 0.1
+p = 1
 
-train_loader, validation_loader = loader('{}/train'.format(data_path), labels_path, batch_size, validation_split, p=p, transform=trans)
+# train_loader, validation_loader = loader('{}/train'.format(data_path), labels_path, batch_size, validation_split, p=p, transform=trans)
+train_loader, validation_loader = load_datasets(batch_size=128, transforms=trans)
 
 model = TestModel()
 model.train()
 
+'''
 state_path = './weights/resnet_model_3/train_0.1.weights'
 state_dict = torch.load(state_path)
 # state_dict.pop('fc1.weight')
@@ -49,6 +52,7 @@ model.load_state_dict(state_dict, strict=False)
 to_deactivate = ['conv1a', 'conv1b', 'conv1c', 'conv2a', 'conv2b', 'conv2c', 'conv3a', 'conv3b', 'conv3c']
 for l in to_deactivate:
     deactivate_layer(model, l)
+'''
 
 loss_function = nn.CrossEntropyLoss()
 optimiser = optim.Adam(model.parameters())
@@ -60,7 +64,7 @@ trial.run(epochs=10)
 results = trial.evaluate(data_key=torchbearer.TEST_DATA)
 print(results)
 
-model_name = 'resnet_model_3a'
+model_name = 'resnet_model_3b'
 weights_path = './weights/{}/'.format(model_name)
 
 if not os.path.exists(weights_path):
